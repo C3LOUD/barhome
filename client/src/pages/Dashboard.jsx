2c032d54cba0cde1122325978ec8a9e065b1b2d4
Tommy <tommy51105110@gmail.com>
2022-10-22 20:39:02 +0800

    Add Design System to Tailwind Config

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
 
 const Dashboard = () => {
-  return <div className="bg-pink-200"></div>;
+  return (
+    <div className="bg-black-100 h-screen w-screen">
+      <div className="w-[1440px] h-screen bg-primary-main">
+        {/* <Sidebar />
+        <Main /> */}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
